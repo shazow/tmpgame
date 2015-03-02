b17660b788efda8c43ba35ea585a7aed99e16246
Andrey Petrov <andrey.petrov@shazow.net>
2015-03-01 19:35:26 -0800

    Moar interfaces.

--- game.go
+++ game.go
@@ -19,32 +19,22 @@ type Attacker interface {
 }
 
 type Defender interface {
+	Alive() bool
 	Defend(a Attacker)
 }
 
-type unit struct {
-	Name   string
-	Path   Waypoint
-	Target *Tile
-	Health int
+type Unit interface {
+	Name() string
+	Position() Tile
+	Defender
 }
 
-func (u *unit) Defend(w weapon) {
-	u.Health -= w.Attack()
-}
-
-type weapon struct {
-	// TODO: We could have different kinds of weapons, like ranged/melee, blunt/sharp, etc.
-	Damage int
-}
-
-func (w weapon) Attack() int {
-	// TODO: Random roll?
-	return w.Damage
-}
+//
+// Implementations below:
+//
 
 type Merchant struct {
-	*unit
+	Unit
 	// Number of goods the merchant is storing
 	Burden int
 	// How much the stored goods are worth
@@ -52,11 +42,51 @@ type Merchant struct {
 }
 
 type Guard struct {
-	*unit
-	*weapon
+	Unit
+	Attacker
 }
 
 type Thief struct {
-	*unit
-	*weapon
+	Unit
+	Attacker
+}
+
+// unit implements Unit, Defender
+type unit struct {
+	name     string
+	path     Waypoint
+	position *Tile
+	target   *Tile
+	health   int
+}
+
+func (u unit) Name() string {
+	return u.name
+}
+
+func (u unit) Position() Tile {
+	return u.position
+}
+
+func (u unit) Target() Tile {
+	return u.target
+}
+
+func (u unit) Alive() bool {
+	return u.health > 0
+}
+
+func (u *unit) Defend(w weapon) {
+	u.health -= w.Attack()
+}
+
+// weapon implements Attacker
+type weapon struct {
+	// TODO: We could have different kinds of weapons, like ranged/melee, blunt/sharp, etc.
+	damage int
+}
+
+func (w weapon) Attack() int {
+	// TODO: Random roll?
+	return w.damage
 }
